7233ce1876abca5e9ecbbe58f5b45715019a60da
Loren Brown <lorenwbrown@gmail.com>
2019-10-18 15:32:26 -0700

    Wrench-catalog service draft

--- wrench-catalog-app/src/app/services/wrench-catalog/wrench-catalog.service.ts
+++ wrench-catalog-app/src/app/services/wrench-catalog/wrench-catalog.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+
+import { WrenchCatalog } from './models/wrench-catalog.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WrenchCatalogService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  GetCatalog(): Observable<HttpResponse<WrenchCatalog[]>> {
+    const apiKey: string = environment.wrenchCatalogServiceApiKey;
+    const endpoint: string = environment.wrenchCatalogServiceEndpoint;
+
+    const customHeaders: HttpHeaders = new HttpHeaders();
+    customHeaders.set('x-api-key', apiKey);
+
+    return this.http.get<WrenchCatalog[]>(endpoint, { observe: 'response', headers : customHeaders });
+  }
 }
